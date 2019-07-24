012c7ba634627b2f5bbc307b7999579df245388e
Gleb Matorin <matorindev@gmail.com>
2019-07-24 11:15:12 +0700

    Initial setup

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+// Compile scss into css
+function style() {
+    // 1. where is my scss file
+    // 2. pass that file through sass compiler
+    // 3. where do I save the compiler CSS?
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+    // 4. stream changes to the browser
+    .pipe(browserSync.stream());
+}
+
+function watch(){
+    browserSync.init({
+        server:{
+            baseDir: './'
+        }
+    });
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./*.html').on('change',browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change',browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
